d7fcff43f08d600f2ca151890f7964dc8a506c47
ahmadstiff <royanuahmad313@gmail.com>
2024-12-19 23:00:01 +0700

    change navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,31 @@
-"use client";
+'use client';
 
-import { cn } from "@/lib/utils";
-import { Menu, Home, AudioWaveform } from "lucide-react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
-import Logo from "./Logo";
-import { ThemeToggle } from "./ThemeToggle";
-import { Button, buttonVariants } from "./ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu";
-import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { cn } from '@/lib/utils';
+import { Menu, Home, AudioWaveform } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import Logo from './Logo';
+import { ThemeToggle } from './ThemeToggle';
+import { Button, buttonVariants } from './ui/button';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from './ui/dropdown-menu';
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 
 const items = [
-  { label: "Home", link: "/", icon: <Home />},
-  { label: "Prediction", link: "/prediction", icon: <AudioWaveform />},
+  { label: 'Home', link: '/', icon: <Home /> },
+  { label: 'Prediction', link: '/prediction', icon: <AudioWaveform /> },
 ];
 
 export default function Navbar() {
   return (
     <>
-      <DesktopNavbar/>
-      <MobileNavbar/>
+      <DesktopNavbar />
+      <MobileNavbar />
     </>
   );
 }
@@ -31,31 +35,22 @@ function MobileNavbar() {
 
   return (
     <div
-      className="absolute z-40 h-fit w-full py-5 md:py-0"
+      className='absolute z-40 h-fit w-full py-5 md:py-0'
       style={{
         backgroundColor: 'rgba(128, 128, 128, 0.2)',
       }}
     >
-      <div className="block md:hidden" >
-        <nav className="container flex items-center justify-between px-8">
-          <Sheet
-            open={isOpen}
-            onOpenChange={setIsOpen}
-          >
+      <div className='block md:hidden'>
+        <nav className='container flex items-center justify-between px-8'>
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger asChild>
-              <Button
-                variant={"ghost"}
-                size={"icon"}
-              >
-                <Menu className="w-8 h-8 shrink-0" />
+              <Button variant={'ghost'} size={'icon'}>
+                <Menu className='w-8 h-8 shrink-0' />
               </Button>
             </SheetTrigger>
-            <SheetContent
-              className="w-[250px] sm:w-[540px]"
-              side={"left"}
-            >
+            <SheetContent className='w-[250px] sm:w-[540px]' side={'left'}>
               <Logo />
-              <div className="flex flex-col gap-1 pt-4">
+              <div className='flex flex-col gap-1 pt-4'>
                 {items.map((item) => (
                   <NavbarItem
                     key={item.label}
@@ -67,7 +62,7 @@ function MobileNavbar() {
               </div>
             </SheetContent>
           </Sheet>
-          <div className="flex items-center gap-2">
+          <div className='flex items-center gap-2'>
             <ThemeToggle />
           </div>
         </nav>
@@ -79,55 +74,51 @@ function MobileNavbar() {
 function DesktopNavbar() {
   return (
     <div
-      className="absolute z-40 h-fit w-full"
+      className='fixed top-0 left-1/2 z-40 h-fit w-1/2 -translate-x-1/2 rounded-lg shadow-lg'
       style={{
-        backgroundColor: 'rgba(128, 128, 128, 0.2)',
+        background:
+          'linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(128, 128, 128, 0.2))',
+        backdropFilter: 'blur(10px)',
       }}
     >
-      <div className="hidden md:block" >
-        <nav className="container flex items-center justify-between gap-x-4 px-20">
+      <div className='hidden md:block'>
+        <nav className='container flex items-center justify-between gap-x-4 px-10 py-2'>
           <Logo />
-          <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
-            <div className="flex items-center h-full gap-x-2 item">
-              {items.map((item) => (
-                item.label === "About" ? (
+          <div className='flex h-[60px] items-center gap-x-4'>
+            <div className='flex items-center gap-x-2'>
+              {items.map((item) =>
+                item.label === 'About' ? (
                   <DropdownMenu key={item.label}>
                     <DropdownMenuTrigger asChild>
                       <Button
-                        variant={"outline"}
-                        className="w-full gap-2 justify-end text-lg text-muted-foreground hover:text-foreground"
+                        variant={'outline'}
+                        className='w-full gap-2 justify-start text-lg text-muted-foreground hover:text-foreground'
                       >
                         {item.icon}
                       </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
                       <DropdownMenuItem>
-                        <Link href="/about">
-                          Tentang Kami
-                        </Link>
+                        <Link href='/about'>Tentang Kami</Link>
                       </DropdownMenuItem>
                       <DropdownMenuItem>
-                        <Link href="/faq">
-                          FAQ
-                        </Link>
+                        <Link href='/faq'>FAQ</Link>
                       </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        Kontak Kami
-                      </DropdownMenuItem>
+                      <DropdownMenuItem>Kontak Kami</DropdownMenuItem>
                     </DropdownMenuContent>
                   </DropdownMenu>
                 ) : (
                   <NavbarItem
                     key={item.label}
                     link={item.link}
-                    label={''}
+                    label={""}
                     icon={item.icon}
                   />
                 )
-              ))}
+              )}
             </div>
           </div>
-          <div className="flex items-center gap-2">
+          <div className='flex items-center gap-2'>
             <ThemeToggle />
           </div>
         </nav>
@@ -139,7 +130,7 @@ function DesktopNavbar() {
 function NavbarItem({
   link,
   label,
-  icon
+  icon,
 }: {
   link: string;
   label: string;
@@ -148,20 +139,20 @@ function NavbarItem({
   const pathname = usePathname();
   const isActive = pathname === link;
   return (
-    <div className="relative flex items-center">
+    <div className='relative flex items-center'>
       <Link
         href={link}
         className={cn(
-          buttonVariants({ variant: "outline" }),
-          "w-full gap-2 justify-start text-lg text-muted-foreground hover:text-foreground",
-          isActive && "text-foreground"
+          buttonVariants({ variant: 'outline' }),
+          'w-full gap-2 justify-start text-lg text-muted-foreground hover:text-foreground',
+          isActive && 'text-foreground'
         )}
       >
         {icon}
         {label}
       </Link>
       {isActive && (
-        <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl dark:bg-primary bg-emerald-500 md:block" />
+        <div className='absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl dark:bg-primary bg-emerald-500 md:block' />
       )}
     </div>
   );
